26ab302ac581390a2f42ff4d7a4e50b3eb34de01
freek <freek@spatie.be>
2016-03-25 12:18:34 +0100

    wip

--- config/laravel-demo-mode.php
+++ config/laravel-demo-mode.php
@@ -2,12 +2,25 @@
 
 return [
 
-    'enabled' => true,
+    /**
+     * This is the master switch to enable demo mode.
+     */
+    'enabled' => env('DEMO_MODE_ENABLED', true),
 
+    /**
+     * Visitors that go an url that is protected by demo mode will be redirected.
+     * to this url
+     */
     'redirect_unauthorized_users_to_url' => '/work-in-progress',
-    
+
+    /**
+     * Visiting this url will grant access to the pages protected by demo mode.
+     */
     'grant_access_to_demo_url' => '/demo',
 
+    /**
+     * After have been granted access visitors will be redirected to this url.
+     */
     'redirect_authorized_users_to_url' => '/',
 
 ];
