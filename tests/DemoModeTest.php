d5e92b6a0a79acb99c8b8f698afd9402d21dffcd
Alex Vanderbist <alex.vanderbist@gmail.com>
2017-09-22 15:10:28 +0200

    Add tests for strict_mode and authorized_ips

--- tests/DemoModeTest.php
+++ tests/DemoModeTest.php
@@ -2,6 +2,8 @@
 
 namespace Spatie\DemoMode\Test;
 
+use Illuminate\Routing\RouteCollection;
+
 class DemoModeTest extends TestCase
 {
     /** @test */
@@ -36,5 +38,54 @@ class DemoModeTest extends TestCase
         $this
             ->get('/unprotected-page')
             ->assertSee('unprotected content');
+    }
+
+    /** @test */
+    public function it_will_automatically_authorize_configured_ips()
+    {
+        $authorizedIps = ['192.0.2.1', '192.0.2.2', '192.0.2.3'];
+
+        $this
+            ->get('/secret-page')
+            ->assertRedirect();
+
+        $this->app['config']->set('demo-mode.authorized_ips', $authorizedIps);
+
+        $this
+            ->call('GET', '/secret-page', [], [], [], ['REMOTE_ADDR' => $authorizedIps[0]])
+            ->assertSee('secret content');
+    }
+
+    /** @test */
+    public function it_will_not_create_the_demo_route_in_strict_mode()
+    {
+        $this->app['config']->set('demo-mode.strict_mode', true);
+
+        $this->setUpRoutes($this->app);
+
+        $this
+            ->get('/demo')
+            ->assertStatus(404);
+    }
+
+    /** @test */
+    public function it_will_only_authorize_configured_ips_with_strict_mode_enabled()
+    {
+        $this->app['config']->set('demo-mode.strict_mode', true);
+
+        $this->setUpRoutes($this->app);
+
+        $this
+            ->get('/secret-page')
+            ->assertRedirect()
+            ->assertHeader('location', $this->config['redirect_unauthorized_users_to_url']);
+
+        $authorizedIps = ['192.0.2.1', '192.0.2.2', '192.0.2.3'];
+
+        $this->app['config']->set('demo-mode.authorized_ips', $authorizedIps);
+
+        $this
+            ->call('GET', '/secret-page', [], [], [], ['REMOTE_ADDR' => $authorizedIps[0]])
+            ->assertSee('secret content');
     }
 }
