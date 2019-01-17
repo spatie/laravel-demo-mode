d451075258f902b6beeb835491b57d695ee596ad
Raúl Cubitto <raulcubitto@gmail.com>
2019-01-17 18:41:25 -0300

    Split test classes for default and custom demo guards
    Add trait with shared tests for both test classes
    Add custom guard test class

---
+++ tests/CustomDemoGuardTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Spatie\DemoMode\Test;
+
+use Spatie\DemoMode\DemoGuardContract;
+
+class CustomDemoGuardTest extends TestCase
+{
+    use TestsShared;
+
+    protected function resolveApplicationConfiguration($app)
+    {
+        parent::resolveApplicationConfiguration($app);
+
+        $app['config']['demo-mode.guard'] = TestDemoGuard::class;
+    }
+
+    /** @test */
+    public function it_will_allow_visiting_secret_pages_if_the_custom_flag_is_set_to_true()
+    {
+        $this->turnFlagOn()
+            ->assertCanVisitSecretPage();
+    }
+
+    /** @test */
+    function it_will_restrict_access_to_secret_pages_if_the_custom_flag_is_set_to_false()
+    {
+        $this->turnFlagOff()
+            ->assertCannotVisitSecretPage();
+    }
+
+    /** @test */
+    public function it_redirects_users_when_accessing_an_unknown_route_if_the_custom_flag_is_off()
+    {
+        $this
+            ->turnFlagOff()
+            ->get('/unknown-page')
+            ->assertRedirect()
+            ->assertHeader('location', $this->config['redirect_unauthorized_users_to_url']);
+    }
+
+    private function turnFlagOn()
+    {
+        $this->app->get(DemoGuardContract::class)->flag = true;
+
+        return $this;
+    }
+
+    private function turnFlagOff()
+    {
+        $this->app->get(DemoGuardContract::class)->flag = false;
+
+        return $this;
+    }
+}
---
+++ tests/DefaultDemoGuardTest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Spatie\DemoMode\Test;
+
+class DefaultDemoGuardTest extends TestCase
+{
+    use TestsShared;
+
+    /** @test */
+    public function it_redirects_users_who_have_not_been_granted_access_to_a_work_in_progress_page()
+    {
+        $this->withoutExceptionHandling();
+
+        $this->assertCannotVisitSecretPage();
+    }
+
+    /** @test */
+    public function it_will_automatically_authorize_configured_ips()
+    {
+        $authorizedIps = ['192.0.2.1', '192.0.2.2', '192.0.2.3'];
+
+        $this->assertCannotVisitSecretPage();
+
+        $this->app['config']->set('demo-mode.authorized_ips', $authorizedIps);
+
+        $this->assertCanVisitSecretPage($authorizedIps[0]);
+    }
+
+    /** @test */
+    public function it_will_only_authorize_configured_ips_with_strict_mode_enabled()
+    {
+        $this->app['config']->set('demo-mode.strict_mode', true);
+
+        $this-> setUpRoutes($this->app);
+
+        $this->assertCannotVisitSecretPage();
+
+        $authorizedIps = ['192.0.2.1', '192.0.2.2', '192.0.2.3'];
+
+        $this->app['config']->set('demo-mode.authorized_ips', $authorizedIps);
+
+        $this->assertCanVisitSecretPage($authorizedIps[0]);
+    }
+
+    /** @test */
+    public function it_will_allow_visiting_secret_pages_after_having_visited_the_grant_demo_url_first()
+    {
+        $this
+            ->get('/demo')
+            ->assertRedirect()
+            ->assertHeader('location', $this->config['redirect_authorized_users_to_url']);
+
+        $this->assertCanVisitSecretPage();
+    }
+
+    /** @test */
+    public function it_redirects_users_who_have_not_been_granted_demo_access_when_accessing_an_unknown_route()
+    {
+        $this
+            ->get('/unknown-page')
+            ->assertRedirect()
+            ->assertHeader('location', $this->config['redirect_unauthorized_users_to_url']);
+    }
+}
